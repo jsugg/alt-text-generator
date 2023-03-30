570876c2f4a8a033c020916e522139ee5e042ccd
Juan Sugg <juanpedrosugg@gmail.com>
2023-03-30 20:40:43 -0300

    Added Swagger at /api-docs and described the API endpoints

--- api/v1/routes/api.js
+++ api/v1/routes/api.js
@@ -17,7 +17,22 @@ module.exports = (serverLogger) => {
   function loadAPIRoutes(serverLogger) {
     serverLogger.logger.info('Loading APIRoutes...');
 
-    // Ping
+    /**
+     * @swagger
+     * /api/ping:
+     *   get:
+     *     summary: Check if the API is available
+     *     responses:
+     *       200:
+     *         description: API is online and listening
+     *         content:
+     *           text/plain:
+     *             schema:
+     *               type: string
+     *               example: pong
+     *       500:
+     *         description: Some server error
+     */
     apiRouter.get(['/ping', '/v1/ping'], (req, res) => {
       req.log.startTime = Date.now();
       req.log.logger.info(`${req.log.startTime} Request received`);
@@ -25,8 +40,33 @@ module.exports = (serverLogger) => {
       res.status(200).send('pong');
     });
 
-    // getImages from external URL
-    apiRouter.get(['/webScrapper/getImages', '/v1/webScrapper/getImages'], cors(), async (req, res) => {
+    /**
+     * @swagger
+     * /api/scrapper/images:
+     *   get:
+     *     summary: Returns the list of images found in a website
+     *     description: This endpoint visits the website, selects the <img> elements, extracts its href attribute, and returns them in JSON format.
+     *     parameters:
+     *       - name: url
+     *         in: query
+     *         description: URL of the website containing the image hrefs to be scrapped
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 imageSources:
+     *                   type: array
+     *                   example: ["https://cdn.statically.io/img/neymarques.com/wp-content/uploads/2022/11/Ney-Simple-LoFi-1024x1024.jpeg","https://cdn.statically.io/img/neymarques.com/wp-content/uploads/2022/11/Spotify-Emblema-e1668647961321-150x150.png"]
+     *       500:
+     *         description: Some server error
+     */
+    apiRouter.get(['/scrapper/images', '/v1/scrapper/images'], cors(), async (req, res) => {
       req.log.startTime = Date.now();
       req.log.logger.info(`${req.log.startTime} Request received`);
       req.log(req, res);
@@ -47,43 +87,48 @@ module.exports = (serverLogger) => {
       }
     });
 
-    // Get Alt Text for images
-    apiRouter.get(['/accessibility/getImageDescriptions', '/v1/accessibility/getImageDescriptions'], cors(), async (req, res) => {
+    /**
+     * @swagger
+     * /api/accessibility/description:
+     *   get:
+     *     summary: Returns a description for a given image
+     *     description: This endpoint takes an image URL, fetches its content, converts it into a data URL, and sends it to an AI service that returns a description for it.
+     *     parameters:
+     *       - name: image_source
+     *         in: query
+     *         description: URL of the image to be described
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   description:
+     *                     type: string
+     *                     example: A man with glasses is playing a violin.
+     *                   imageUrl:
+     *                     type: string
+     *                     example: https://neymarques.com/wp-content/uploads/2022/12/IMG_2752-2.jpg
+     *       500:
+     *         description: Some server error
+     */
+    apiRouter.get(['/accessibility/description', '/v1/accessibility/description'], cors(), async (req, res) => {
       req.log.startTime = Date.now();
       req.log.logger.info(`${req.log.startTime} Request received`);
       req.log(req, res);
 
-      const imagesSource = req.query.imagesSource;
-      const model = req.query.model;
-      req.log.logger.debug(`Model: ${model}, imagesSource: ${imagesSource}`);
-
-      if (!imagesSource || !model) {
-        res.status(400).json({ error: 'Missing required query parameter(s): imagesSources and model are required.' });
-        return;
-      }
-      if (model === 'clip') {
-        try {
-            const descriptions = await ReplicateImageDescriber.describeImages(imagesSource);
-            res.json(descriptions);
-            req.log.logger.debug(`Response sent with descriptions:`);
-        } catch (error) {
-          res.status(500).json({ error: 'Error fetching descriptions for the provided images' });
-        }
-      }
-    });
-
-    // Get Alt Text for (one) image
-    apiRouter.get(['/accessibility/getImageDescription', '/v1/accessibility/getImageDescription'], cors(), async (req, res) => {
-      req.log.startTime = Date.now();
-      req.log.logger.info(`${req.log.startTime} Request received`);
-      req.log(req, res);
-
-      const imageSource = req.query.imageSource;
+      const imageSource = req.query.image_source;
       const model = req.query.model;
       req.log.logger.debug(`Model: ${model}, imageSource: ${imageSource}`);
 
       if (!imageSource || !model) {
-        res.status(400).json({ error: 'Missing required query parameter(s): imageSource and model are required.' });
+        res.status(400).json({ error: 'Missing required query parameter(s): image_source and model are required.' });
         return;
       }
       if (model === 'clip') {
