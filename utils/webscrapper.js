3365f94b0b4dd70ba48d27ea56a29f7b82528012
Juan Sugg <juanpedrosugg@gmail.com>
2023-04-11 04:40:48 -0400

    Refactored webscrapper module

--- utils/webscrapper.js
+++ utils/webscrapper.js
@@ -1,81 +1,92 @@
-"use strict"
-const axios = require('axios');
-const cheerio = require('cheerio');
-const url = require('url');
+"use strict";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const url = require("url");
 
 class WebScrapper {
+  static isImage(imageUrl) {
+    const imageExtensions = [
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".gif",
+      ".bmp",
+      ".webp",
+      ".svg",
+    ];
+    return imageExtensions.some((ext) =>
+      imageUrl.toLowerCase().split("?")[0].endsWith(ext)
+    );
+  }
 
-    static isImage(url) {
-        const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.svg'];
-        return imageExtensions.some(ext => url.toLowerCase().split('?')[0].endsWith(ext));
+  static async fetchHtml(targetUrl) {
+    try {
+      const response = await axios.get(targetUrl, {
+        headers: {
+          Origin: targetUrl,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching the website:", error);
     }
+    return null;
+  }
 
-    static async getImages(targetUrl) {
-        try {
-          // Fetch the HTML content of the website using Axios
-          const response = await axios.get(targetUrl, {
-            headers: {
-              'Origin': targetUrl
-            }
-          });
-      
-          // Parse the HTML using Cheerio
-          const $ = cheerio.load(response.data);
-      
-          // Extract the src attribute of all img elements
-          const images = [];
-          const possibleAttributes = [
-            'data-src',
-            'data-original-src',
-            'data-lazy-src',
-            'data-srcset',
-            'src'
-          ];
+  static extractImageSources(html, targetUrl) {
+    const $ = cheerio.load(html);
+    const images = [];
+    const possibleAttributes = [
+      "data-src",
+      "data-original-src",
+      "data-lazy-src",
+      "data-srcset",
+      "src",
+    ];
 
-          $('img').each((index, img) => {
-            const imgAttributes = $(img)[0].attribs;
-            //const imgAttributes = $(img).attribs;
-            let src;
+    $("img").each((index, img) => {
+      const imgAttributes = $(img)[0].attribs;
+      let src;
 
-            // First, try the common attributes
-            for (const attr of possibleAttributes) {
-              const candidateSrc = imgAttributes[attr];
-              if (candidateSrc && WebScrapper.isImage(candidateSrc)) {
-                src = candidateSrc.split('?')[0];
-                break;
-              }
-            }
-
-            // If not found, search the rest of the attributes
-            if (!src) {
-              for (const attr in imgAttributes) {
-                if (possibleAttributes.includes(attr)) continue;
-
-                const candidateSrc = imgAttributes[attr];
-                if (candidateSrc && WebScrapper.isImage(candidateSrc)) {
-                  src = candidateSrc;
-                  break;
-                }
-              }
-            }
-
-            if (src) {
-              // Resolve relative URLs to absolute URLs
-              src = url.resolve(targetUrl, src);
-              images.push(src);
-            }
-          });
-
-          // Log the images array
-          console.log('Images:', images);
-          const imageSources = { "imageSources": images };
-          // Return the images array
-          return imageSources;
-
-        } catch (error) {
-          console.error('Error fetching the website:', error);
+      for (const attr of possibleAttributes) {
+        const candidateSrc = imgAttributes[attr];
+        if (candidateSrc && WebScrapper.isImage(candidateSrc)) {
+          src = candidateSrc.split("?")[0];
+          break;
         }
-    }
+      }
+
+      if (!src) {
+        for (const attr in imgAttributes) {
+          if (possibleAttributes.includes(attr)) continue;
+
+          const candidateSrc = imgAttributes[attr];
+          if (candidateSrc && WebScrapper.isImage(candidateSrc)) {
+            src = candidateSrc;
+            break;
+          }
+        }
+      }
+
+      if (src) {
+        src = url.resolve(targetUrl, src);
+        images.push(src);
+      }
+    });
+
+    return { imageSources: images };
+  }
+
+  static async getImages(targetUrl) {
+    const html = await WebScrapper.fetchHtml(targetUrl);
+    if (!html) return null;
+    const imageSources = WebScrapper.extractImageSources(html, targetUrl);
+    console.log("Images:", imageSources.imageSources);
+    return imageSources;
+  }
 }
 
 module.exports = WebScrapper;
+
+// Path: utils/scraper.js
